0762f1eb6eaf534e7b002d4642f8543f729baa50
Giovanna Oliveira <giovannabeathryce.pso@gmail.com>
2023-01-18 18:11:58 -0300

    adding field validators

--- src/components/FormContainer/index.jsx
+++ src/components/FormContainer/index.jsx
@@ -1,26 +1,38 @@
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import Schema from '../../validators';
 import './style.scss'
 
 const FormContainer = () => {
 
-    const { register, handleSubmit} = useForm()
+    const { register, handleSubmit, formState:{errors} } = useForm({
+        resolver: yupResolver(Schema)
+    })
+
+    const registerData = (data) => {
+        console.log(data);
+    }
 
     return(
         <section className='req'>
             <h1>Simule sua Antecipação</h1>
-            <form>
-                <label>
+            <form onSubmit={handleSubmit(registerData)}>
+                <label htmlFor='amount'>
                     Informe o valor da sua venda *
-                    <input type='text'{...register('amount')}></input>
+                    <input id='amount' type='text' placeholder='Ex: 1500.0'{...register('amount')}></input>
+                    <p className='err'>{errors.amount?.message}</p>
                 </label>
-                <label>
+
+                <label htmlFor='installments'>
                     Em quantas parcelas *
-                    <input type='text' {...register('installments')} ></input>
-                <span>máximo de 12 parcelas</span>
+                    <input id='installments' type='number' {...register('installments')} ></input>
+                    <p className='err'>{errors.installments?.message}</p>
                 </label>
-                <label>
+
+                <label htmlFor='mdr'>
                     Informe o percentual MDR *
-                    <input type='text' {...register('mdr')}></input>
+                    <input id='mdr' type='text' {...register('mdr')}></input>
+                    <p className='err'>{errors.mdr?.message}</p>
                 </label>
                 
                 <button>Calcular</button>
